10339023b8c6286f4fafb5b89eb2bd15b059d4f9
Ronny Valdivieso <rvaldivieso@pacificsoft.net>
2020-04-23 15:41:20 -0500

    Footer: Source modified

--- src/containers/TheFooter.vue
+++ src/containers/TheFooter.vue
@@ -1,10 +1,10 @@
 <template>
   <CFooter :fixed="false">
     <div>
-      <span>Desarrollado por <strong>Ronny Valdivieso.</strong></span>
+      <span>Developed by <strong>Ronny Valdivieso.</strong></span>
     </div>
     <div class="ml-auto">
-      <span class="mr-1">Fuente de datos:</span>
+      <span class="mr-1">This report was developed using the</span>
       <a href="https://covid19api.com/" target="_blank">COVID 19 API</a>
     </div>
   </CFooter>
