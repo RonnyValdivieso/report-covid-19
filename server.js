3a5c41a6bd177e0b57e0bab1615c3f3eceb8ab8b
Ronny Valdivieso <rvaldivieso@pacificsoft.net>
2020-04-23 02:56:04 -0500

    Deploy fix

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import serveStatic from 'serve-static';
 const path = require('path');
-app = express();
-app.use(serveStatic(path.join(__dirname, 'dist')));
 const port = process.env.PORT || 80;
-app.listen(port);
+
+express()
+	.use(serveStatic(path.join(__dirname, 'dist')))
+	.listen(port);
